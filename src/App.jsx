d4effef0686c25331c8fa79637c7e4930853c52d
ffozilbek <fozilbekfayzullayev05@gmail.com>
2025-03-07 23:35:36 +0500

    JWT token saved Localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
 import { Route, Routes } from "react-router-dom";
 import { Main, Navbar, LogIn, Register } from "./components/";
+import { useEffect } from "react";
+import AuthService from "./servise/auth";
+import { useDispatch } from "react-redux";
+import { signinUserSuccess } from "./slice/auth";
+import { getItem } from "./helpers/persistance-store";
 
 function App() {
+  const dispatch = useDispatch();
+
+  const getUser = async () => {
+    try {
+      const response = await AuthService.getUser();
+      dispatch(signinUserSuccess(response.user));
+    } catch (error) {
+      console.log("error");
+    }
+  };
+
+  useEffect(() => {
+    const token = getItem("token");
+    if (token) {
+      getUser();
+    }
+  }, []);
+
   return (
     <div>
       <Navbar />
--- src/servise/auth.js
+++ src/servise/auth.js
@@ -11,6 +11,7 @@ const AuthService = {
   },
   async getUser() {
     const response = await axios.post("/user");
+    return response.data;
   },
 };
 
